880de4cd4f9ba4c77ce6d31505e293b765774443
HannahRoseGit <rozhana@kth.se>
2023-01-21 18:11:49 +0100

    hopefully last update

--- taking_derivative/deriv.ex
+++ taking_derivative/deriv.ex
@@ -12,32 +12,53 @@ defmodule Derivative do
     # by writers Robert A. Adams and Christopher Essex.
 
     # 1. derivative of a constant function = 0
-    def deriv({:num,_},_) do {:num, 0} end
+    def deriv({:num,_},_) do 
+        {:num, 0} 
+    end
 
     # 2. derivation of a variable with no coefficient 
-    def deriv({:var,x},x) do {:num, 1} end
+    def deriv({:var,x},x) do 
+        {:num, 1} 
+    end
 
     # 3. derivation of a variable by another variable 
-    def deriv({:var,x},_) do {:var,x} end
+    def deriv({:var,x},_) do 
+        {:var,x} 
+    end
 
     # 4. derivation general power rule 
-    def deriv({:pow, e1, {:num, n}}, e1) do {:mul, {:num, n}, {:pow, e1, {:num, n-1}}} end
+    def deriv({:pow, e1, {:num, n}}, e1) do 
+        {:mul, {:num, n}, {:pow, e1, {:num, n-1}}} 
+    end
 
     # 5. derivattion of squar root
-    def deriv({:sqrt, e1}, x) do {:frac, deriv(e1,x), {:mul, {:num, 2}, {:sqrt, e1}}} end
+    def deriv({:sqrt, e1}, x) do 
+        {:frac, deriv(e1,x), {:mul, {:num, 2}, {:sqrt, e1}}} 
+    end
 
     # 6. derivattion of Ln
-    def deriv({:ln, x}, x) do {:frac, {:num, 1}, x} end
+    def deriv({:ln, x}, x) do 
+        {:frac, {:num, 1}, x} 
+    end
 
     # 7. derivattion of multiplication 
-    def deriv({:mul, e1, e2}, x) do {:add, {:mul, deriv(e1,x), e2}, {:mul, e1, deriv(e2,x)}} end 
+    def deriv({:mul, e1, e2}, x) do 
+        {:add, {:mul, deriv(e1,x), e2}, {:mul, e1, deriv(e2,x)}} 
+    end 
 
     # 8. derivattion of addition 
-    def deriv({:add, e1, e2}, x) do {:add, deriv(e1,x), deriv(e2,x)} end 
+    def deriv({:add, e1, e2}, x) do 
+        {:add, deriv(e1,x), deriv(e2,x)} 
+    end 
 
     # 9. derivattion of trigonometery 
-    def deriv({:sin, e1}, x) do {:mul, deriv(e1,x), {:cos, e1}} end
-    def deriv({:cos, e1}, x) do {:mul, {:num, -1}, {:mul, {deriv(e1,x), {:sin, e1}}}} end 
+    def deriv({:sin, e1}, x) do 
+        {:mul, deriv(e1,x), {:cos, e1}} 
+    end
+
+    def deriv({:cos, e1}, x) do 
+        {:mul, {:num, -1}, {:mul, {deriv(e1,x), {:sin, e1}}}} 
+    end 
 
     # 10. derivation of fraction 
     def deriv({:frac, e1, e2}, x) do {:frac, {:sub, {:mul, deriv(e1,x), e2}, {:mul, deriv(e2, x), e1}}, {:pow, e2, 2}} end
@@ -84,22 +105,94 @@ defmodule Derivative do
     def simp({:var, :x}) do {:var, :x} end 
 
     #addition
-    def simp({:add, e1, e2}) do simp_add(simp(e1), simp(e2)) end
-    def simp_add(e1, {:num, 0}) do e1 end
-    def simp_add({:num, 0}, e1) do e1 end  
-    def simp_add({:num, n1}, {:num, n2}) do {:num, n1+n2} end
-    def simp_add({:var, v}, {:var, v}) do {:mul, {:num, 2}, {:var, v}} end
+    def simp({:add, e1, e2}) do 
+        simp_add(simp(e1), simp(e2)) 
+    end
+
+    def simp_add(e1, {:num, 0}) do 
+        e1 
+    end
+
+    def simp_add({:num, 0}, e1) do 
+        e1 
+    end  
+
+    def simp_add({:num, n1}, {:num, n2}) do 
+        {:num, n1+n2} 
+    end
+
+    def simp_add({:var, v}, {:var, v}) do 
+        {:mul, {:num, 2}, {:var, v}} 
+    end
 
     #multiplication
-    def simp({:mul, e1, e2})  do simp_mul(simp(e1), simp(e2)) end
-    def simp_mul(_, {:num,0}) do {:num, 0} end
-    def simp_mul({:num, 0}, _) do {:num, 0} end
-    def simp_mul(e1, {:num,1}) do e1 end
-    def simp_mul({:num, 1}, e1) do e1 end
-    #def simp_mul(simp(e1), simp(e2)) do {:mul, e1*e2} end
+    def simp({:mul, e1, e2})  do
+        simp_mul(simp(e1), simp(e2)) 
+    end
 
+    def simp_mul(_, {:num,0}) do 
+        {:num, 0} 
+    end
+
+    def simp_mul({:num, 0}, _) do 
+        {:num, 0} 
+    end
+
+    def simp_mul(e1, {:num,1}) do
+        e1 
+    end
+
+    def simp_mul({:num, 1}, e1) do 
+        e1 
+    end
+
+    def simp_mul({:num, n1}, {:mul, {:num, n2}, e2}) do
+        {:mul, {:num, n1*n2}, e2}
+    end
+
+    def simp_mul({:num, n1}, {:mul, e2, {:num, n2}}) do
+        {:mul, {:num, n1*n2}, e2}
+    end
+
+    def simp_mul({:mul, {:num, n1}, e1}, {:num, n2}) do
+        {:mul, {:num, n1*n2}, e1}
+    end
+
+    def simp_mul({:mul, e1, {:num, n1}}, {:num, n2}) do
+        {:mul, {:num, n1*n2}, e1}
+    end
+
+    def simp_mul({:num, n}, {:num, m}) do 
+        {:num, n*m} 
+    end
+
+    def simp_mul({:var, v}, {:var, v}) do 
+        {:pow, {:var, v}, {:num, 2}} 
+    end
+
+    def simp_mul({:var, v}, {:pow, {:var, v}, {:num, n}}) do 
+        {:pow, {:var, v}, {:num, n+1}} 
+    end  
+
+    def simp_mul({:pow, {:var, v}, {:num, n}}, {:var, v}) do 
+        {:pow, {:var, v}, {:num, n+1}} 
+    end
+
+    #exponential
+    def simp_pow(_,{:num, 0}) do
+        1 
+    end  
+
+    def simp_pow(e1,{:num, 1}) do  
+        e1 
+    end
+
+    def simplify_pow(e1,e2) do
+        {:pow, e1, e2}
+    end
+    
 end
 
 Derivative.test()
-IO.puts("Please be proud of yourself")
+IO.puts("You came a long way. Please be proud of yourself")
 
